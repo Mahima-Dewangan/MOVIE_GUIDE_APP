292371ee18e5589c566e159bfba93e600c9c5166
Mahima-Dewangan <mahimadewangan11@gmail.com>
2025-10-25 13:01:28 +0530

    second push

---
+++ script.js
@@ -0,0 +1,7 @@
+// acessing all the dom elements
+const searchForm = document.querySelector('form');
+const movieContainer = document.querySelector('.movie-container');
+const moviePoster = document.querySelector('.movie-poster');
+const movieDetails = document.querySelector('.movie-details');
+
+
